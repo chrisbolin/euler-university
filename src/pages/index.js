a327edbd041eb2c7179fd82d1cb771362b976777
Chris Bolin <bolin.chris@gmail.com>
2020-02-09 21:14:46 -0700

    simple table of contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,40 @@
 import React from "react";
-import { Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Welcome to Euler University</h1>
-    <p>
-      Lessons in computer science and math from Project Euler. There are no
-      spoilers and no solutions.
-    </p>
-    <ul>
-      <li>
-        <Link to="/1">Problem 1</Link>
-      </li>
-    </ul>
-  </Layout>
-);
-
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Welcome to Euler University</h1>
+      <p>
+        Lessons in computer science and math from Project Euler. There are no
+        spoilers and no solutions.
+      </p>
+      <ul>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <li key={node.frontmatter.path}>
+            <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 export default IndexPage;
