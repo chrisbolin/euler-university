4f6aa5e43e0269fd9d4aec2bd354fa5fb817f19a
Chris Bolin <bolin.chris@gmail.com>
2020-02-09 21:04:21 -0700

    prettier on *.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-const path = require(`path`)
+const path = require(`path`);
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/markdownTemplate.js`)
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`src/templates/markdownTemplate.js`);
   const result = await graphql(`
     {
       allMarkdownRemark(limit: 1000) {
@@ -14,17 +14,17 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         }
       }
     }
-  `)
+  `);
   // Handle errors
   if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
   }
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.path,
       component: blogPostTemplate,
-      context: {}, // additional data can be passed via context
-    })
-  })
-}
+      context: {} // additional data can be passed via context
+    });
+  });
+};
